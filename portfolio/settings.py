cee9bac3df7c4f45cc4860e41d1828af6e5d9203
Aa1696 <sonumahtoraj121@gmail.com>
2022-05-28 12:39:05 +0530

    Modifed the settings.py of database configured to postgresql from dqsqlite

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Port_Folio_DB',
+        'USER':'postgres',
+        'PASSWORD':'Aa@13421696',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
